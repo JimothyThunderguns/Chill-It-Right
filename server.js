f232570abe76e199ed75ddcd1ed78d4dcba455a9
Ryan Jackson <rcjackson96@gmail.com>
2022-11-16 22:53:12 -0600

    reverting changes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // Require Dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 // Initializiing the express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+<<<<<<< HEAD
 // data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +20,23 @@ require('./routes/routes')(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
+=======
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//
+app.use(express.static)
+
+//Setting up the data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+//requiring the routes file
+require('./routes/routes');
+
+//listener setup
+app.listen(PORT, () => {
+        console.log ('App listening to PORT : ${PORT}')
+});
+>>>>>>> parent of fb7eabc (server.js changes)
