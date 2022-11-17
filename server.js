d35fd2227afd57e84361297e8c312d918fa42b16
Ryan Jackson <rcjackson96@gmail.com>
2022-11-16 23:02:15 -0600

    Revert "reverting changes"
    This reverts commit f232570abe76e199ed75ddcd1ed78d4dcba455a9.

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // Require Dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
 // Initializiing the express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 // data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,23 +19,3 @@ require('./routes/routes')(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
-=======
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-//
-app.use(express.static)
-
-//Setting up the data parsing
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-app.use(express.static(__dirname));
-
-//requiring the routes file
-require('./routes/routes');
-
-//listener setup
-app.listen(PORT, () => {
-        console.log ('App listening to PORT : ${PORT}')
-});
->>>>>>> parent of fb7eabc (server.js changes)
